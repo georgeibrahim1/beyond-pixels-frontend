13d7fb4fa7987d373253b1044fd2a747127544b4
George Ibrahim Aziz <103423847+georgeibrahim1@users.noreply.github.com>
2025-10-02 21:31:24 +0300

    feat: change routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,16 @@ import GamePage from './pages/gamePage'
 import StoryPage from './pages/storyPage'
 import AboutPage from './pages/aboutPage'
 import ErrorPage from './pages/errorPage'
-import UsersPage from './pages/usersPage'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout/>}>
-        <Route path="/home" element={<HomePage/>}/>
-        <Route path="/explore" element={<ExplorePage/>}/>
-        <Route path="/game" element={<GamePage/>}/>
-        <Route path="game/users" element={<UsersPage/>} />
-        <Route path="/story" element={<StoryPage/>}/>
-        <Route path="/about" element={<AboutPage/>}/>
+        <Route index element={<HomePage/>}/>
+        <Route path="explore" element={<ExplorePage/>}/>
+        <Route path="game" element={<GamePage/>}/>
+        <Route path="story" element={<StoryPage/>}/>
+        <Route path="about" element={<AboutPage/>}/>
         <Route path="*" element={<ErrorPage/>}/>
       </Route>
     </Routes>
