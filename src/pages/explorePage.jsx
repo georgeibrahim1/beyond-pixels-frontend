19288c6488fe049b5699aef6239f2dc97fc053b7
George Ibrahim Aziz <103423847+georgeibrahim1@users.noreply.github.com>
2025-10-03 09:50:52 +0300

    feat: add home and explore page contents

--- src/pages/explorePage.jsx
+++ src/pages/explorePage.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./ExplorePage.css";
 
 export default function ExplorePage() {
   return (
-    <div>Ee</div>
-  )
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      eventhandlers={{
+
+      }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://66fa9acb70cc.ngrok-free.app/tiles/{z}/{x}/{y}.jpg"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          <span className="gradient-text text-glow">
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </span>
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
